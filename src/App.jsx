2c779e964e2fe750cd59621d6cd4a1fece32d7ea
sebuahsemangat <mrsyntax25@gmail.com>
2024-05-30 16:19:56 +0700

    Jadi web resep masakan wkwkwk

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,19 @@ function App() {
   return (
     <main className='flex flex-col min-h-[80vh] 
     justify-center items-center max-w-xl w-full mx-auto'>
-      <h1 className="text-4xl text-indigo-500">Cet JIPITI</h1>
+      <h1 className="text-4xl text-indigo-500">Resep GPT</h1>
       
       <form className='flex flex-col gap-4 py-4 w-full'>
-        <input 
+        <textarea id="content" className='py-2 px-4 text-md rounded-md border-2' wrapLongLines={false}></textarea>
+        {/* <input 
             placeholder='Ketik permintaan'
             id='content'
             type='text'
-            className='py-2 px-4 text-md rounded-md border-2'/>
+            className='py-2 px-4 text-md rounded-md border-2'/> */}
         <button 
             type='button'
             onClick={handleSubmit}
-            className='bg-indigo-500 py-2 px-4 font-bold text-white rounded-md'>Kirim</button>
+            className='bg-indigo-500 py-2 px-4 font-bold text-white rounded-md'>Rekomendasikan Resep!</button>
       </form>
       <div className='max-w-xl w-full mx-auto'>
 
